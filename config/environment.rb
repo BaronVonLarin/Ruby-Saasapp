ca38034d09e46f69a6c1e4456eca1d2798d71bf9
Larin Pawluk <larin.pawluk@gmail.com>
2017-12-03 18:07:13 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ['MAILGUN_SMTP_PORT'], 
+  :address        => ['MAILGUN_SMTP_SERVER'],
+  :user_name      => ['MAILGUN_SMTP_LOGIN'],
+  :password       => ['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'whispering-springs-51303.herokuapp.com/'
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
