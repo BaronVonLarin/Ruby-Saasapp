8f0ecc54cdb976083e7804bc826090edad06771f
Larin Pawluk <larin.pawluk@gmail.com>
2017-12-03 22:25:55 +0000

    reverting

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,9 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      name = params.to_s[:contact][:name]
-      email = params.to_s[:contact][:email]
-      body = params.to_s[:contact][:comments]
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
       ContactMailer.contact_email(name, email, body).deliver
       flash[:success] = "Message Sent."
       redirect_to new_contact_path
