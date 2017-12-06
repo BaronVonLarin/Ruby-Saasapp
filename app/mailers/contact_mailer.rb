936dae6e0ccdbd8904f4a9a66c873beb0ff8f286
Larin Pawluk <larin.pawluk@gmail.com>
2017-12-06 23:54:58 +0000

    Removed body variable from controller, mailer, contact.rb  and email template

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,9 +2,9 @@ class ContactMailer < ActionMailer::Base
   default to: 'vonpoops@gmail.com'   
   
   def contact_email(name, email, body)
-    @name = name.to_s
-    @email = email.to_s
-    @body = body.to_s
+    @name = name
+    @email = email
+    #@body = body
     
     mail(from: email, subject: 'Contact Form Message')
   end 
